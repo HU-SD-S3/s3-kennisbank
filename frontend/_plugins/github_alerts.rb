8abc2dde54c55284d04edd85e48e63e64b5d84df
Alex.Jongman <Alex.Jongman@hu.nl>
2026-02-06 15:48:37 +0100

    Refactor GithubAlertsConverter to improve HTML conversion and update section margin in style.scss

--- frontend/_plugins/github_alerts.rb
+++ frontend/_plugins/github_alerts.rb
@@ -1,13 +1,13 @@
 module Jekyll
   class GithubAlertsConverter
     def convert(content)
-      # Convert [!NOTE], [!WARNING], etc. to proper HTML
-      content.gsub!(/<blockquote>\n<p>\[!(NOTE|TIP|IMPORTANT|WARNING|CAUTION)\](.*?)<\/p>\n(.*?)<\/blockquote>/m) do |match|
-        alert_type = $1.downcase
-        rest = $3
-        
-        %(<blockquote data-type="#{alert_type}">#{rest}</blockquote>)
-      end
+      # Convert [!NOTE], [!WARNING], etc. to proper HTML with data-type attribute
+      # Matches blockquote with [!TYPE] at the start
+      content.gsub!(%r{<blockquote>\s*<p>\[!(NOTE|TIP|IMPORTANT|WARNING|CAUTION)\](.+?)</p>}, 
+                   '<blockquote data-type="\1.downcase">\n<p>\2</p>')
+      
+      content.gsub!(%r{<blockquote>\s*<p>\[!(note|tip|important|warning|caution)\](.+?)</p>}, 
+                   '<blockquote data-type="\1">\n<p>\2</p>')
       
       content
     end
